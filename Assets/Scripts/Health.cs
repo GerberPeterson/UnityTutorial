861d68db7e565b53f9bc6195958ad2e7070dc08f
Gerber Peterson <gerber.peterson@gmail.com>
2018-06-01 17:27:36 -0700

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    private const int maxHealth = 50;
+
+    private float healthBarScale;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    public bool destroyOnDeath;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    void Start()
+    {
+        healthBarScale = healthBar.rect.width / maxHealth;
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+
+                // called on the Server, but invoked on the Clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth * healthBarScale, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if(isLocalPlayer)
+        {
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick a spawn point at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                int randy = Random.Range(0, spawnPoints.Length);
+
+                string debugMessage = System.String.Format("Randy chose {0}", randy);
+
+                Debug.Log(debugMessage);
+
+                spawnPoint = spawnPoints[randy].transform.position;
+            }
+
+            // Set the player's position to the chosen spawn point
+            transform.position = spawnPoint;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    // Adjustable parameters
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletSpeed;
+    public float playerRunSpeed;
+    public float playerStrafeSpeed;
+    public float playerTurnSpeed;
+
+    // Camera parameters
+    Vector3 cameraPosition = new Vector3(2.5f, 1.5f, -3f);
+    Vector3 cameraRotation = new Vector3(10f, 0f, 0f);
+    float cameraFOV = 90f;
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+
+        // Set up the camera on spawn
+        if (isLocalPlayer)
+        {
+            Camera cameraObject = FindObjectOfType<Camera>();
+            if (cameraObject != null)
+            {
+                SetUpCamera(cameraObject);
+            }
+            else
+            {
+                Debug.Log("Couldn't find MainCamera in scene.");
+            }
+        }
+    }
+
+    private void SetUpCamera(Camera cameraObject)
+    {
+        cameraObject.transform.parent = gameObject.transform;
+        cameraObject.transform.localPosition = cameraPosition;
+        cameraObject.transform.localRotation = Quaternion.Euler(cameraRotation);
+        cameraObject.fieldOfView = cameraFOV;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+
+        var turn = Input.GetAxis("Turn") * Time.deltaTime * playerTurnSpeed;
+        var run = Input.GetAxis("Vertical") * Time.deltaTime * playerRunSpeed;
+        var strafe = Input.GetAxis("Horizontal") * Time.deltaTime * playerStrafeSpeed;
+
+        transform.Rotate(0, turn, 0);
+        transform.Translate(strafe, 0, run);
+	}
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
